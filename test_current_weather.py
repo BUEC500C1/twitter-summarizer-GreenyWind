38e0dc89e2d6987b13b0ec63496addd787f58888
GreenyWind <whelzyq@163.com>
2020-02-08 22:07:25 -0500

    test search_by_name & search_by_coordinates

--- test_current_weather.py
+++ test_current_weather.py
@@ -1,15 +1,19 @@
-from weather_APIs.apweather import search_by_name, search_by_coordinates
+from apweather import search_by_name, search_by_coordinates
 
-def test_search_fail():
+def test_fail():
 	respond = search_by_name('TT')
 	assert respond['status'] == 'Fail'
 
 	respond = search_by_coordinates(1000, 10)
 	assert respond['status'] == 'Fail'
 
-def test_search_success():
+def test_success():
 	respond = search_by_name('London')
 	assert respond['status'] == 'Success'
 
 	respond = search_by_coordinates(40, 74)
 	assert respond['status'] == 'Success'
+
+if __name__ == "__main__":
+	test_fail()
+	test_success()
