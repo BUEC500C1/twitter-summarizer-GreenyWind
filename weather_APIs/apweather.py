dbc1a57d304cb7e3a0d2810ea51c89dc490b0cd6
GreenyWind <whelzyq@163.com>
2020-02-08 20:57:40 -0500

    weather apis

---
+++ weather_APIs/apweather.py
@@ -0,0 +1,36 @@
+import requests as req
+
+def F_to_C(temp_F):
+    return ((temp_F - 32) / 1.8)
+
+def search_by_name(city_name, state=None, country_abbreviation=None):
+    respond['status'] = 'Fail'
+    if not type(city_name) == str:
+        return respond
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city_name
+    if state!=None and country_abbreviation!=None:
+        url = url + ',' + state + ',' + country_abbreviation
+    elif state!=None:
+        url = url + ',' + state
+    elif country_abbreviation!=None:
+        url = url + ',' + country_abbreviation
+    url = url + '&appid=65fca6cee9f758f6bbabffdd03b8e7d6'
+    r = req.get(url)
+    if r.status_code != 200:
+        return respond
+    r = r.json()
+    respond.update('coord', r['coord'])
+    respond.update("weather", r["weather"][0]["main"])
+    respond.update("description", r["weather"][0]["description"])
+    respond.update("tempreture", F_to_C(r["main"]["temp"]))
+    respond.update("temp_min", F_toC(r["main"]["temp_min"]))
+    respond.update("temp_max", F_toC(r["main"]["temp_max"]))
+    respond.update("humidity", r["main"]["humidity"])
+    respond.update("wind", r["wind"]["speed"])
+    return respond
+
+def search_by_coordinates(latitude, longitude):
+
+def forecast_by_name(city_name, state=None, country_abbreviation=None):
+
+def forecast_by_coordinates(latitude, longitude):
