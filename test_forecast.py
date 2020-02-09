a6028f5cf1d585d5ded900edf4840f5b545e34ad
GreenyWind <whelzyq@163.com>
2020-02-08 22:29:12 -0500

    Delete test_forecast.py

--- test_forecast.py
+++
@@ -1,9 +0,0 @@
-from weather_APIs.apweather import forecast_by_coordinates
-
-def test_fail():
-	respond = forecast_by_coordinates(74, 10)
-	assert respond['status'] == 'Fail'
-
-def test_success():
-	respond = forecast_by_coordinates(39.7456, -97.0892)
-	assert respond['status'] == 'Success'
