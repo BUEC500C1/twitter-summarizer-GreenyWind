e1b7ced23ef60a9f64e993ddcf8718a96b63192f
GreenyWind <whelzyq@163.com>
2020-02-08 21:23:33 -0500

    test forecast_by_name & forecast_by_coordinates

--- test_forecast.py
+++ test_forecast.py
@@ -1,13 +1,13 @@
 from weather_APIs.apweather import forecast_by_name, forecast_by_coordinates
 
-def test_fail():
+def test_forecast_fail():
 	respond = forecast_by_name('New York')
 	assert respond['status'] == 'Fail'
 
 	respond = forecast_by_coordinates(1000, 10)
 	assert respond['status'] == 'Fail'
 
-def test_success():
+def test_forecast_success():
 	respond = forecast_by_name('London')
 	assert respond['status'] == 'Success'
 
